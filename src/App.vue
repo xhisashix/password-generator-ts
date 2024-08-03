24b4a1fccef9f04cfdd47fcf974e5d64e0ec757c
Hisashi Iwamoto <hisasi622@gmail.com>
2024-08-03 21:19:08 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container my-4 mx-auto">
+    <h1 class="text-3xl">Vue 3 + Vite + TypeScript</h1>
+  </div>
+</template>
+
+<style scoped></style>
